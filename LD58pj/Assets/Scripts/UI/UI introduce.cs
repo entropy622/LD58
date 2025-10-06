7ae1bd26b6e6b54c2093921a94451c40113939a7
xinpingqihe-creator <15118067760@163.com>
2025-10-06 11:49:29 +0800

    introduce

---
+++ LD58pj/Assets/Scripts/UI/UI introduce.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class UIintroduce : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+
+    {
+        //给这个物体添加一个文本对象
+        GameObject textObj = new GameObject("WorldText");
+        textObj.transform.SetParent(transform);
+        textObj.transform.localPosition = new Vector3(0, -50, 0);
+        TextMeshProUGUI text = textObj.AddComponent<TextMeshProUGUI>();
+        text.text = "drag to change the order of abilities" + "\n" + gettext();
+        text.fontSize = 24;
+        text.alignment = TextAlignmentOptions.Center;
+        //设置成白色
+        text.color = Color.white;
+
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        // 删除文本对象
+        GameObject textObj = GameObject.Find("WorldText");
+        if (textObj != null)
+        {
+            Destroy(textObj);
+        }
+
+    }
+    
+    private string gettext()
+    {
+        string name = gameObject.name;
+        switch (name)
+        {
+            case "Movement":
+                return "Movement: Move left and right";
+            case "Jump":
+                return "Jump: Jump up";
+            case "IronBlock":
+                return "IronBlock: ??";
+            case "IceBlock":
+                return "IceBlock: Increase your speed";
+            case "GravityFlip":
+                return "GravityFlip: Flip gravity to walk on the ceiling";
+            case "Dash":
+                return "Dash: Quickly dash in the direction you are facing";
+            case "Balloon":
+                return "Balloon: Create a balloon to slow your fall";
+            default:
+                return "";
+        }
+    }
+    
+    
+}
